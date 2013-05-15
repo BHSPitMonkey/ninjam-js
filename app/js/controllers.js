d6e50d6148e8b2f1ffc57da5a4799a4615c3d171
Stephen Eisenhauer <bhs2007@gmail.com>
2013-05-15 03:09:05 -0500

    Buncha changes. Bad commit messages much?

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('ChatPane', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('UsersPane', [function() {
 
   }]);
