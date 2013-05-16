bc56770178d0531d570a4443ddc3a94d304c7377
Stephen Eisenhauer <bhs2007@gmail.com>
2013-05-16 04:47:40 -0500

    More progress. Things are firing double, though.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,29 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('ChatPane', [function() {
-
-  }])
-  .controller('UsersPane', [function() {
-
-  }]);
+  controller('ServerBrowser', function($scope, NinjamClient) {
+    $scope.ninjam = NinjamClient;
+    
+    $scope.visible = true;
+    $scope.opts = {
+      backdropFade: true,
+      dialogFade: true
+    };
+    
+    $scope.defaultUsername = "NinjamJSUser";
+    $scope.host = "ninbot.com:2049";
+    
+    $scope.connect = function() {
+      console.log("You tried connecting!");
+      NinjamClient.connect($scope.host, $scope.defaultUsername, null);
+    };
+  }).
+  controller('HeaderPane', function($scope, NinjamClient) {
+    $scope.ninjam = NinjamClient;
+  }).
+  controller('ChatPane', function($scope, NinjamClient) {
+    $scope.ninjam = NinjamClient;
+  }).
+  controller('UsersPane', function($scope, NinjamClient) {
+    $scope.ninjam = NinjamClient;
+  });
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,52 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  factory('NinjamClient', function($timeout) {
+    var NinjamClient = function() {
+      this.socketId = null;
+      this.status = "starting";
+      this.host = null;
+      this.port = null;
+      this.users = [];
+      this.bpm = null;
+      this.bpi = null;
+      
+      // Try to create the socket
+      console.log("Trying to create socket...");
+      chrome.socket.create('tcp', {}, this._onCreate.bind(this));
+    };
+    
+    angular.extend(NinjamClient.prototype, {
+      
+      // Connect to specified Ninjam server
+      connect : function(host, username, password) {
+        if (this.socketId > 0) {
+          console.log("You're trying to connect! This client's socket ID is " + this.socketId + " and status is " + this.status);
+        }
+        else {
+          console.log("Can't connect: Socket not created!");
+        }
+      },
+      
+      // Send something to server
+      send : function() {
+        
+      },
+      
+      // Called when socket gets created
+      _onCreate : function(createInfo) {
+        console.log("Called onCreate. Got socket ID " + createInfo.socketId);
+        this.socketId = createInfo.socketId;
+        if (this.socketId > 0) {
+          this.status = "ready";
+        }
+        else {
+          console.log("Couldn't create socket!");
+          this.status = "no socket";
+        }
+      },
+    });
+    
+    return new NinjamClient();
+  });
