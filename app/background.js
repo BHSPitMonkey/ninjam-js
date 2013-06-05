6bf7f6e2f4bf6f30a26675ca4048d43f0a89f5f6
Stephen Eisenhauer <bhs2007@gmail.com>
2013-06-05 16:32:54 -0500

    Window resizing limitations

--- app/background.js
+++ app/background.js
@@ -1,6 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'width': 800,
-    'height': 700
+    'height': 700,
+    'minWidth': 600,
+    'minHeight': 400,
   });
 });
